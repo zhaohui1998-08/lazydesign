68b91e9ea71b24f8a5e71549597a54ed3129ea43
zhaohui <42004177@qq.com>
2023-01-05 17:22:24 +0800

    "搭框架"

---
+++ src/main/java/com/lazydesign/LazydesignApplication.java
@@ -0,0 +1,13 @@
+package com.lazydesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazydesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazydesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lazydesign/LazydesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazydesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazydesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
