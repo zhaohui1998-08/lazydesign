d81fd6478437ab3be463a2baf4ab29bc1688e464
zhaohui <42004177@qq.com>
2024-01-10 16:44:03 +0800

    Revert "test取消提交"
    This reverts commit 7fd4f77835a958524b556ca8807128522d0f2dfc.

--- src/main/java/com/lazydesign/controller/UserController.java
+++ src/main/java/com/lazydesign/controller/UserController.java
@@ -4,13 +4,11 @@ package com.lazydesign.controller;
 import com.lazydesign.entity.English;
 import com.lazydesign.entity.User;
 import com.lazydesign.service.UserService;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.ibatis.annotations.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -31,15 +29,6 @@ public class UserController {
         int i = userService.addUser(user);
         return i;
     };
-
-    @PostMapping("/login")
-    public Result login(User user, HttpServletRequest request) {
-
-        System.out.println(1);
-        return null;
-    };
-
-
 
 
 }
