7fd4f77835a958524b556ca8807128522d0f2dfc
zhaohui <42004177@qq.com>
2024-01-10 16:43:16 +0800

    test取消提交

--- src/main/java/com/lazydesign/controller/UserController.java
+++ src/main/java/com/lazydesign/controller/UserController.java
@@ -4,11 +4,13 @@ package com.lazydesign.controller;
 import com.lazydesign.entity.English;
 import com.lazydesign.entity.User;
 import com.lazydesign.service.UserService;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.ibatis.annotations.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -29,6 +31,15 @@ public class UserController {
         int i = userService.addUser(user);
         return i;
     };
+
+    @PostMapping("/login")
+    public Result login(User user, HttpServletRequest request) {
+
+        System.out.println(1);
+        return null;
+    };
+
+
 
 
 }
